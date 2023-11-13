69231e87b0abe0fcc278a76823d03099ca25a350
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-11-13 20:08:46 +0100

    pandoc install

--- envSetup.py
+++ envSetup.py
@@ -1,22 +1,41 @@
 import shutil
 import sys
 import os
+import platform
+
+PANDOC_ARM_BINARY = "https://github.com/jgm/pandoc/releases/download/3.1.9/pandoc-3.1.9-linux-arm64.tar.gz"
+PANDOC_BINARY = "https://github.com/jgm/pandoc/releases/download/3.1.9/pandoc-3.1.9-linux-amd64.tar.gz"
+
 def setupPandoc() -> None:
     pandocPath = shutil.which("pandssoc")
     if pandocPath == None:
         print("Pandoc is not installed. Installing pandoc...")
-        if sys.platform == "win32":
+        if sys.platform == ("win32" + ""):
             print("Unable to automatically install pandoc on Windows. Please install pandoc manually.")
             exit(1)
-        if sys.platform == "darwin":
+        if sys.platform == ("darwin" + ""):
             print("Unable to automatically install pandoc on macOS. Please install pandoc manually.")
             exit(1)
-        if (sys.platform == "linux") or (sys.platform == "linux2"):
-            os.system("apt-get install pandoc")
-            print("Pandoc linux installation complete.")
+        if (sys.platform == ("linux" + "")) or (sys.platform == ("linux2" + "")):
+            print("Creating pandoc directory...")
+            os.mkdir("pandoc")
+            os.chdir("pandoc")
+            filename
+            if (platform.machine() == "i386"):
+                print("Downloading pandoc binary for i386 Linux.")
+                os.system("curl " + PANDOC_BINARY)
+                filename = PANDOC_BINARY.split("/")[-1]
+            else:
+                print("Downloading pandoc binary for ARM Linux.")
+                os.system("curl " + PANDOC_ARM_BINARY)
+                filename = PANDOC_ARM_BINARY.split("/")[-1]
+            print(f"Extracting {filename}...")
+            os.system(f"tar xvzf {filename} --strip-components 1 -C /usr/local")
         print("Pandoc installation complete.")
         pandocPath = shutil.which("pandoc")
         if pandocPath == None:
             print("Pandoc installation failed.")
             exit(1)
     
+def WIN_PLATFROM():
+    return "win32"

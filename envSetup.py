c181368da5de3898e81a9f830ed4ad5d95692ac7
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-11-27 13:14:33 +0100

    changed permissions

--- envSetup.py
+++ envSetup.py
@@ -19,7 +19,7 @@ def setupPandoc() -> None:
             exit(1)
         if (sys.platform == ("linux" + "")) or (sys.platform == ("linux2" + "")):
             print("Creating pandoc directory...")
-            os.mkdir("pandoc", 0o755)
+            os.mkdir("pandoc", 0o777)
             os.chdir("pandoc")
             filename = ""
             if (platform.machine() == "i386"):

59db087fcc9209bff2800699fd189200c739d1ed
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-12-29 15:22:28 +0100

    switch build script to latex

--- document/build.py
+++ document/build.py
@@ -2,21 +2,11 @@ import envSetup as setup
 import os
 
 def build() -> None:
-    if not setup.setupPandoc(): return None
     os.chdir("document")
     os.chdir("src")
-    out = ""
-    with open("title.md", "r", encoding="utf-8") as title:
-        out = out +  "".join(title.readlines())
-    with open("content.md", "r", encoding="utf-8") as content:
-        out = out + "".join(content.readlines()).replace("# ", "\\newpage\n# ")
 
-    with open("final.md", "w", encoding="utf-8") as content:
-        content.write(out)
-
-    os.system("pandoc final.md -s -f markdown -o ../../static/showcase.pdf -H preamble.tex") 
-
-    os.remove("final.md")
+    # Build the document
+    os.system("pdflatex ./index.tex --output-directory=../../dist/showcase")
 
     os.chdir("..")
     os.chdir("..")
